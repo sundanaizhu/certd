e6ab0b6864ab0c09a05968b04fa5383851699810
xiaojunnuo <xiaojunnuo@qq.com>
2024-11-04 10:38:15 +0800

    chore: simple nanoid 12位

--- packages/core/basic/src/utils/util.id.ts
+++ packages/core/basic/src/utils/util.id.ts
@@ -1,3 +1,3 @@
 import { customAlphabet } from 'nanoid';
 
-export const simpleNanoId = customAlphabet('1234567890abcdefghijklmopqrstuvwxyz', 10);
+export const simpleNanoId = customAlphabet('1234567890abcdefghijklmopqrstuvwxyz', 12);
