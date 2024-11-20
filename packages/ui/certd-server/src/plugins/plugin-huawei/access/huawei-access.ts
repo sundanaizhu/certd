de43391e4c12dc3ad976f8fa8787f4eb70a41e75
xiaojunnuo <xiaojunnuo@qq.com>
2024-11-20 13:58:41 +0800

    perf: 华为云密钥获取提示及访问链接

--- packages/ui/certd-server/src/plugins/plugin-huawei/access/huawei-access.ts
+++ packages/ui/certd-server/src/plugins/plugin-huawei/access/huawei-access.ts
@@ -11,7 +11,7 @@ export class HuaweiAccess extends BaseAccess {
     component: {
       placeholder: 'accessKeyId',
     },
-    helper: '证书申请需要有dns解析权限',
+    helper: '证书申请需要有dns解析权限，前往[我的凭证-访问密钥](https://console.huaweicloud.com/iam/?region=cn-east-3#/mine/accessKey)获取',
     required: true,
   })
   accessKeyId = '';
