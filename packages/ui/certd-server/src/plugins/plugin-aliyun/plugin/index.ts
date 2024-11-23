c96fcb7afced979435cffa73591275008033c90d
xiaojunnuo <xiaojunnuo@qq.com>
2024-11-24 01:40:42 +0800

    perf: 支持部署到阿里云WAF

--- packages/ui/certd-server/src/plugins/plugin-aliyun/plugin/index.ts
+++ packages/ui/certd-server/src/plugins/plugin-aliyun/plugin/index.ts
@@ -2,3 +2,4 @@ export * from './deploy-to-cdn/index.js';
 export * from './deploy-to-dcdn/index.js';
 export * from './deploy-to-oss/index.js';
 export * from './upload-to-aliyun/index.js';
+export * from './deploy-to-waf/index.js';
